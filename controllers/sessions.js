a70b3cad65eeadfbe5f502f998fb4508f3523f9d
brian-cooney <bcooney@gmail.com>
2017-04-25 15:08:20 +0100

    starting to do the sessions

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -8,9 +8,12 @@ function sessionsCreate(req, res) {
   User
     .findOne({ email: req.body.email })
     .then((user) => {
-      if (!user || !user.validatePassword(req.body.password)) {
+      if(!user || !user.validatePassword(req.body.password)) {
         return res.status(401).render('sessions/new', { message: 'Unrecognised credentials' });
       }
+
+      req.session.userId = user.id;
+
       return res.redirect('/');
     });
 }
--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const express         = require('express');
-const expressLayouts  = require('express-ejs-layouts');
-const bodyParser      = require('body-parser');
-const mongoose        = require('mongoose');
-const methodOverride  = require('method-override');
-const env             = require('./config/env');
-const router          = require('./config/routes');
-const app             = express();
+// require modules
+const express        = require('express');
+const morgan         = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose       = require('mongoose');
+mongoose.Promise     = require('bluebird');
+const routes         = require('./config/routes');
+const User           = require('./models/user');
+const session        = require('express-session');
+const flash          = require('express-flash');
 
-mongoose.connect(env.db);
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+const env = require('./config/env');
 
+// setup Express app
+const app = express();
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
+// setup database
+mongoose.connect(env.db);
+
+// middleware
+app.use(morgan('dev'));
 app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride((req) => {
+
+app.use(methodOverride(function (req) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     const method = req.body._method;
     delete req.body._method;
@@ -23,6 +36,38 @@ app.use(methodOverride((req) => {
   }
 }));
 
-app.use(router);
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'ssh it\'s a secret',
+  resave: false,
+  saveUninitialized: false
+}));
 
-app.listen(env.port, () => console.log(`Server up and running on port: ${env.port}.`));
+app.use(flash());
+
+app.use((req, res, next) => {
+  console.log(req.session.userId);
+  if (!req.session.userId) return next();
+
+  User
+    .findById(req.session.userId)
+    .exec()
+    .then((user) => {
+      if(!user) {
+        return req.session.regenerate(() => {
+          req.flash('danger', 'You must be logged in.');
+          res.redirect('/');
+        });
+      }
+
+      // Re-assign the session id for good measure
+      req.session.userId = user._id;
+
+      res.locals.user = user;
+      res.locals.isLoggedIn = true;
+
+      next();
+    });
+});
+
+app.use(routes);
+app.listen(env.port, () => console.log(`Express is listening on port ${env.port}`));
